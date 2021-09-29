bf8356b392ce22dc4c3b861965203f3c39643fb6
perwinbausas <60432447+perwinbausas@users.noreply.github.com>
2021-09-29 20:48:32 +0800

    Add files via upload

---
+++ js/MainController/MainController.js
@@ -0,0 +1,12 @@
+app.controller('MainController', ['$scope', function($scope){
+	$scope.greeting = "Credit Card Payment";
+}])
+
+app.controller('cardnum', ['$scope', function($scope){
+}]);
+
+app.controller('expdate', ['$scope', function($scope){
+}]);
+
+app.controller('cvc', ['$scope', function($scope){
+}]);
